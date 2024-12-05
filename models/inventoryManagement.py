662414a9979a7642a9cc11f8a4cceb0149b83537
ayato1027 <k22235kk@aitech.ac.jp>
2024-12-05 15:23:25 +0900

    modelの編集とroutesの追加

--- models/inventoryManagement.py
+++ models/inventoryManagement.py
@@ -2,7 +2,7 @@ from peewee import Model, ForeignKeyField, IntegerField
 from .db import db
 from order import Order 
 
-class InventoryManagement():
+class InventoryManagement(Model):
     product = ForeignKeyField(Order, backref='anagements')  # 製品名を外部キーとして関連付け
     quantity = IntegerField()  # 在庫の個数を保存
     class Meta:
