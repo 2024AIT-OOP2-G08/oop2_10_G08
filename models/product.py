009a33f4fce041ba11eec4366564621f18a26ecf
ayato1027 <k22235kk@aitech.ac.jp>
2024-12-09 01:15:46 +0900

    inventory_listの追加とその他の修正

--- models/product.py
+++ models/product.py
@@ -1,10 +1,10 @@
-from peewee import Model, CharField, DecimalField
+from peewee import Model, CharField, DecimalField, IntegerField
 from .db import db
 
 class Product(Model):
     name = CharField()
     price = DecimalField()
-    # quantity = DecimalField()
+    quantity = IntegerField(default=0)  # デフォルト値を設定
 
     class Meta:
         database = db
--- routes/__init__.py
+++ routes/__init__.py
@@ -2,11 +2,13 @@ from .user import user_bp
 from .product import product_bp
 from .order import order_bp
 from .review import review_bp
+from .inventory import inventory_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
   product_bp,
   order_bp,
-  review_bp
+  review_bp,
+  inventory_bp
 ]
