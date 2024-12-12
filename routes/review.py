f799a88964d5a20abdcccf3bd30466fb09dafb29
ayato1027 <k22235kk@aitech.ac.jp>
2024-12-12 16:03:38 +0900

    review.pyの修正

--- routes/review.py
+++ routes/review.py
@@ -10,20 +10,10 @@ review_bp = Blueprint('review', __name__, url_prefix='/reviews')
 
 
 
-@review_bp.route('/', methods=['GET', 'post'])
+@review_bp.route('/', methods=['GET', 'POST'])
 def list():
-    # 製品ごとにレビュー数を集計
-    query = (
-        Review
-        .select(Product.name, fn.SUM(Review.review_count).alias('total_reviews'))
-        .join(Product, on=(Review.product_id == Product.id))
-        .group_by(Product.name)
-    )
-
-    # テンプレートで利用できるようにデータを準備
-    items = [{"name": row.product.name, "review_count": row.total_reviews} for row in query]
-
-    return render_template("review_list.html", title="レビュー一覧", items=items)
+    review = Review.select()
+    return render_template("review_list.html", title="レビュー", items = review)
 
 @review_bp.route('/add', methods=['GET', 'POST'])
 def add():
@@ -56,3 +46,18 @@ def edit(review_id):
     users = User.select()
     products = Product.select()
     return render_template('review_edit.html', review=review, users=users, products=products)
+
+@review_bp.route('/graph', methods=['GET', 'post'])
+def graph():
+    # 製品ごとにレビュー数を集計
+    query = (
+        Review
+        .select(Product.name, fn.SUM(Review.review_count).alias('total_reviews'))
+        .join(Product, on=(Review.product_id == Product.id))
+        .group_by(Product.name)
+    )
+
+    # テンプレートで利用できるようにデータを準備
+    items = [{"name": row.product.name, "review_count": row.total_reviews} for row in query]
+
+    return render_template("review_graph.html", title="レビュー一覧", items=items)
