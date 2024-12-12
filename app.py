cf9d84ef08326f62ecd9411f4c2bbfb16a374bd9
matsudakatio <77916609+matsudakatio@users.noreply.github.com>
2024-12-12 15:38:49 +0900

    トップページにレビューの合計数を表示

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+from peewee import fn  # 集計のために使用
+from models import Review, User, Product
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,7 +17,18 @@ for blueprint in blueprints:
 # ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    query = (
+        Review
+        .select(Product.name, fn.SUM(Review.review_count).alias('total_reviews'))
+        .join(Product, on=(Review.product_id == Product.id))
+        .group_by(Product.name)
+    )
+
+    # テンプレートで利用できるようにデータを準備
+    items = [{"name": row.product.name, "review_count": row.total_reviews} for row in query]
+
+    return render_template("index.html", title="レビュー一覧", items=items)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
